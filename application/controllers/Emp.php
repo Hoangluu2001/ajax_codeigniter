3a0bff2acdf73e13249afb26861e634b152bf204
hoangluu2001 <hoangthehoaluu@gmail.com>
2021-06-07 11:20:42 +0700

    crud bang ajax codeigniter

---
+++ application/controllers/Emp.php
@@ -0,0 +1,30 @@
+<?php
+class Emp extends CI_Controller{
+	function __construct(){
+		parent::__construct();
+        // echo ('1');
+		$this->load->model('EmpModel');
+        // $this->load->library('session');
+        $this->load->helper('url');
+
+	}
+	function index(){
+		$this->load->view('listEmp');
+	}
+	function show(){
+		$data=$this->EmpModel->employeeList();
+		echo json_encode($data);
+	}
+	function save(){
+		$data=$this->EmpModel->saveEmp();
+		echo json_encode($data);
+	}
+	function update(){
+		$data=$this->EmpModel->updateEmp();
+		echo json_encode($data);
+	}
+	function delete(){
+		$data=$this->EmpModel->deleteEmp();
+		echo json_encode($data);
+	}
+}
